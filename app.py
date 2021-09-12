a05b6cf93d18388f17ea8004d54d7db18457f7ce
yakubovskyigor <igorby8881@gmail.com>
2021-09-13 00:07:16 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Главная страница"
+
+
+@app.route('/orders')
+def order():
+    return "Заказы"
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == "GET":
+        return jsonify({"response": "Get Request Called"})
+    elif request.method == "POST":
+        req_Json = request.json
+        name = req_Json['name']
+        return jsonify({"response": "Hi" + name})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
