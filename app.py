978c4bd0bf9da052a4f722351519caeb0a39945f
yakubovskyigor <igorby8881@gmail.com>
2021-09-17 07:37:55 +0300

    add user

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ client = pymongo.MongoClient(host="localhost", port=27017)
 coffee = client.coffee
 users = coffee.users
 
+
 @app.route('/')
 def index_page():
     return "Главная страница"
@@ -18,6 +19,12 @@ def index_page():
 @app.route('/orders')
 def order():
     return "Заказы"
+
+
+@app.route('/add')
+def add():
+    users.insert_one({"name": "Mark"})
+    return jsonify("name")
 
 
 @app.route('/test', methods=['GET', 'POST'])
@@ -30,78 +37,42 @@ def test():
         return jsonify({"response": "Hi " + name})
 
 
-@app.route("/", methods=['post', 'get'])
-def index():
-    message = ''
-    if "email" in session:
-        return redirect(url_for("logged_in"))
-    if request.method == "POST":
-        user = request.form.get("fullname")
-        email = request.form.get("email")
-
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
-
-        user_found = users.find_one({"name": user})
-        email_found = users.find_one({"email": email})
-        if user_found:
-            message = 'There already is a user by that name'
-            return jsonify(message=message)
-        if email_found:
-            message = 'This email already exists in database'
-            return jsonify(message=message)
-        if password1 != password2:
-            message = 'Passwords should match!'
-            return jsonify(message=message)
-        else:
-            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
-            user_input = {'name': user, 'email': email, 'password': hashed}
-            users.insert_one(user_input)
-
-            user_data = users.find_one({"email": email})
-            new_email = user_data['email']
-
-            return jsonify(email=new_email)
-    return redirect(url_for('/'))
-
-
-@app.route('/logged_in')
+@app.route('/logged_in', methods=["POST"])
 def logged_in():
-    if "email" in session:
-        email = session["email"]
-        return jsonify(email=email)
+    email = request.form["email"]
+    check = users.find_one({"email": email})
+    if check:
+        return jsonify(message="User Exist")
     else:
-        return redirect(url_for("login"))
+        photo = request.form["photo"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
+        email = request.form["email"]
+        date_of_birth = request.form["date_of_birth"]
+        gender = request.form["gender"]
+        user_info = dict(first_name=first_name, last_name=last_name, password=password, phone_number=phone_number,
+                         email=email, photo=photo, date_of_birth=date_of_birth, gender=gender)
+        users.insert_one(user_info)
+        return jsonify(message="User added successfully")
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["POST"])
 def login():
-    message = 'Please login to your account'
-    if "email" in session:
-        return redirect(url_for("logged_in"))
+    if request.is_json:
+        email = request.json["email"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
 
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-
-        email_found = users.find_one({"email": email})
-        if email_found:
-            email_val = email_found['email']
-            passwordcheck = email_found['password']
-
-            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
-                return redirect(url_for('logged_in'))
-            else:
-                if "email" in session:
-                    return redirect(url_for("logged_in"))
-                message = 'Wrong password'
-                return jsonify(message=message)
-        else:
-            message = 'Email not found'
-            return jsonify(message=message)
-    return jsonify(message=message)
+    check = users.find_one({"email": email, "password": password})
+    if check:
+        return jsonify(message="You are registered")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
